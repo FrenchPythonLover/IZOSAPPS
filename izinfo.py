425bd6dc3f9dbcf4d907dd0d8431551680da4d1b
FrenchPythonLover <117096932+FrenchPythonLover@users.noreply.github.com>
2025-02-07 22:58:21 +0100

    Create izinfo.py

---
+++ izinfo.py
@@ -0,0 +1,24 @@
+import izos, tup, data, time, infosys if isroot:
+print("Starting dump...")
+time.sleep(1)
+dump = f"**
+Sha256_userhash
+Sha256_roothash
+Hostname
+Name
+[data-passwdl
+{data.rootpasswd}
+(data.hostname)
+(data.name)
+##### INFO.PY
+Version
+VerType Release
+Eng
+PythonVersion
+(infosys.version}
+(infosys.vertype)
+(infosys.release)
+{infosys.eng)
+(infosys-pyversion}
+ """
+print(dump)
